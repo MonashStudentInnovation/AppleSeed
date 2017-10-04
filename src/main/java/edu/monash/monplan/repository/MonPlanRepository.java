2908628001d04de071742d1a162ec70b36ddeac8
darvid7 <bvdarvid@gmail.com>
2017-10-04 16:19:33 +1100

    Refactor naming

--- src/main/java/edu/monash/monplan/repository/MonPlanRepository.java
+++ src/main/java/edu/monash/monplan/repository/MonPlanRepository.java
@@ -14,7 +14,6 @@ public class MonPlanRepository<T extends DataModel> extends StringRepository<T> 
     // TODO: Make sure this works.
     public MonPlanRepository(Class<T> classType, SearchConfig searchConfig, String codeField, String nameField) {
         super(classType, searchConfig);
-
         this.codeField = codeField;
         this.nameField = nameField;
     }
@@ -48,9 +47,5 @@ public class MonPlanRepository<T extends DataModel> extends StringRepository<T> 
         return this.put(modelInstance);
     }
 
-
-    public T update(T modelInstance) {
-        return modelInstance;
-    }
 
 }
